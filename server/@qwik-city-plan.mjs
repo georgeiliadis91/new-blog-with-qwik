721adf1be06395d8fe4e9a289d27e91889a5b27a
georgeiliadi91 <george.iliadis.91@gmail.com>
2023-12-01 13:33:38 +0200

    added build and node server

---
+++ server/@qwik-city-plan.mjs
@@ -0,0 +1,1 @@
+import{o as r,p as t,q as o,n as l,r as m,m as n,t as u}from"./assets/@qwik-city-plan-4a357a6a.mjs";import"showdown";export{r as basePathname,t as cacheModules,o as default,l as menus,m as routes,n as serverPlugins,u as trailingSlash};
---
+++ server/entry.ssr.mjs
@@ -0,0 +1,1 @@
+import"./assets/@qwik-city-plan-4a357a6a.mjs";import{r as p}from"./assets/entry.ssr-0d223b6c.mjs";import"showdown";export{p as default};
